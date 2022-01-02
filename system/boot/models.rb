4ee82f608b3c1dfaa3dd9b57ef9d6a2b59f212bf
Hariton <yourrasik@gmail.com>
2022-01-02 12:07:40 +1000

    Add interactive console

--- system/boot/models.rb
+++ system/boot/models.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# This file contains configuration for Sequel Models
+# This file contains configuration for Sequel Models.
 
 Application.boot(:models) do
   init do
@@ -9,30 +9,31 @@ Application.boot(:models) do
 
   start do
     # Whether association metadata should be cached in the association reflection.
-    # If not cached, it wil be computed on demand.
+    # If not cached, it will be computed on demand.
     # In general you only want to set this to false when using code reloading.
     # When using code reloading, setting this will make sure that if an associated class is removed or modified,
     # this class will not have a reference to the previous class.
     Sequel::Model.cache_associations = false if Application.env == 'development'
 
-    # The validation_helpers plugin contains validate_* method designed to be called inside Model#validate to perform validations.
+    # The validation_helpers plugin contains validate_* methods designed to be called inside Model#validate to perform validations.
     Sequel::Model.plugin(:auto_validations)
+
+    # The validation_helpers plugin contains validate_* methods designed to be called inside Model#validate to perform validations.
     Sequel::Model.plugin(:validation_helpers)
 
-    # The prepared_statements plugin modifies the model
-    # to use prepared statement for instance level inserts and updates
+    # The prepared_statements plugin modifies the model to use prepared statements for instance level inserts and updates.
     Sequel::Model.plugin(:prepared_statements)
 
-    # Allows easy access all models subclasses and descendent classes.
+    # Allows easy access all model subclasses and descendent classes.
     Sequel::Model.plugin(:subclasses) unless Application.env == 'development'
 
     # Creates hooks for automatically setting create and update timestamps.
     Sequel::Model.plugin(:timestamps)
 
-    # Allows you to use named timezones instead of just :local and :utc (requires TZInfo)
-    Sequel::Model.extension(:named_timezones)
+    # Allows you to use named timezones instead of just :local and :utc (requires TZInfo).
+    Sequel.extension(:named_timezones)
 
-    # Use UTC time zone for saving time inside db
+    # Use UTC time zone for saving time inside database.
     Sequel.default_timezone = :utc
 
     # Freeze all descendent classes. This also finalizes the associations for those classes before freezing.
