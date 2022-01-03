0a5505a5c35eaeaad80ccc0445a47a6169d127fa
Hariton <yourrasik@gmail.com>
2022-01-03 11:00:58 +0000

    Add: implement authentication_token_generator

---
+++ spec/lib/authentication_token_generator_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe AuthenticationTokenGenerator do
+  let(:token) { described_class.call }
+  let(:user)  { create(:user)        }
+
+  it 'returns token' do
+    expect(token).not_to be_blank
+    expect(token).not_to eq user.authentication_token
+  end
+end
